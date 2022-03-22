8730920654b5dff8a864f9b9e53a08cfae33b57d
Hugo Mota <hugo.mota@ambisig.com>
2022-03-21 22:28:36 -0300

    Creation First configuration.

---
+++ creation/src/main/java/projectmanagement/creation/CreationApplication.java
@@ -0,0 +1,13 @@
+package projectmanagement.creation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreationApplication.class, args);
+	}
+
+}
